e9ae321bb90e1cb70e9f594eb3b245c0a922ed49
Peyton Sterling <peyton@kpstudio.co.uk>
2021-12-06 17:53:31 +0000

    Remembered that arrays exist

--- 2021/Day 06/6b-solution.js
+++ 2021/Day 06/6b-solution.js
@@ -2,29 +2,23 @@ const fs = require("fs");
 let inputText = fs.readFileSync("./6-input.txt", "utf-8");
 let array = inputText.split(",").map((e) => parseInt(e));
 
-var numberGroups = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0 };
+var numberGroups = Array(9).fill(0);
 
 array.forEach((number) => {
-  numberGroups[`${number}`]++;
+  numberGroups[number]++;
 });
 
-console.log(numberGroups);
-
 for (let i = 0; i < 256; i++) {
-  var newNumberGroups = {};
+  var newNumberGroups = [];
+
+  for(let j = 0; j < 8; j++) {
+    newNumberGroups[j] = numberGroups[j + 1]
+  }
 
   newNumberGroups[8] = numberGroups[0];
-  newNumberGroups[7] = numberGroups[8];
-  newNumberGroups[6] = numberGroups[7] + numberGroups[0];
-  newNumberGroups[5] = numberGroups[6];
-  newNumberGroups[4] = numberGroups[5];
-  newNumberGroups[3] = numberGroups[4];
-  newNumberGroups[2] = numberGroups[3];
-  newNumberGroups[1] = numberGroups[2];
-  newNumberGroups[0] = numberGroups[1];
+  newNumberGroups[6] += numberGroups[0];
 
   numberGroups = newNumberGroups;
-  console.log(numberGroups);
 }
 
 var numberOfNumbers = Object.values(numberGroups).reduce(
