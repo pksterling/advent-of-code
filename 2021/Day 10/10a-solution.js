4792106e4dcbf52e3f29fd7274e46a69ac8e0f5c
Peyton Sterling <peyton@kpstudio.co.uk>
2021-12-10 12:27:38 +0000

    Day 10

--- 2021/Day 10/10a-solution.js
+++ 2021/Day 10/10a-solution.js
@@ -1,9 +1,44 @@
 const fs = require("fs");
-let inputText = fs.readFileSync("./9-input.txt", "utf-8");
-let area = inputText
-  .split("\r\n")
-  .map((row) => row.split("").map(Number));
+const { listenerCount } = require("process");
+let inputText = fs.readFileSync("./10-input.txt", "utf-8");
+let input = inputText.split("\r\n").map((row) => row.split(""));
+let openingChars = ["(", "[", "{", "<"];
 
+let matchOpenClose = (open, close) =>
+  (open == "(" && close == ")") ||
+  (open == "[" && close == "]") ||
+  (open == "{" && close == "}") ||
+  (open == "<" && close == ">");
 
+let checkLine = (line, illegalChars) => {
+  let openChars = [];
 
-console.log(riskFactor);
+  for (i in line) {
+    if (openingChars.includes(line[i])) {
+      openChars.unshift(line[i]);
+    } else {
+      if (matchOpenClose(openChars[0], line[i])) {
+        openChars.shift();
+      } else {
+        illegalChars[line[i]]++;
+        break;
+      }
+    }
+  }
+
+  return illegalChars;
+};
+
+let calculateScore = (illegalChars) =>
+  3 * illegalChars[")"] +
+  57 * illegalChars["]"] +
+  1197 * illegalChars["}"] +
+  25137 * illegalChars[">"];
+
+let illegalChars = { ")": 0, "]": 0, "}": 0, ">": 0 };
+
+input.forEach((line) => {
+  illegalChars = checkLine(line, illegalChars);
+});
+
+console.log(calculateScore(illegalChars));
---
+++ 2021/Day 10/10b-solution.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const { listenerCount } = require("process");
+let inputText = fs.readFileSync("./10-input.txt", "utf-8");
+let input = inputText.split("\r\n").map((row) => row.split(""));
+
+let matchOpenClose = (open, close) => {
+  return (
+    (open == "(" && close == ")") ||
+    (open == "[" && close == "]") ||
+    (open == "{" && close == "}") ||
+    (open == "<" && close == ">")
+  );
+};
+
+let getClose = (open) => {
+  if (open == "(") {
+    return ")";
+  }
+  if (open == "[") {
+    return "]";
+  }
+  if (open == "{") {
+    return "}";
+  }
+  if (open == "<") {
+    return ">";
+  }
+};
+
+let processLine = (line) => {
+  let openChars = [];
+
+  for (i in line) {
+    if (line[i] == "(" || line[i] == "[" || line[i] == "{" || line[i] == "<") {
+      openChars.unshift(line[i]);
+    } else {
+      if (matchOpenClose(openChars[0], line[i])) {
+        openChars.shift();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  let completionString = [];
+
+  openChars.forEach((char) => {
+    completionString.push(getClose(char));
+  });
+
+  return completionString;
+};
+
+let calculateScore = (completionString) => {
+  let score = 0;
+
+  completionString.forEach((char) => {
+    let points = char == ")" ? 1 : char == "]" ? 2 : char == "}" ? 3 : 4;
+
+    score = score * 5 + points;
+  });
+
+  return score;
+};
+
+let completionStrings = input.map((line) => processLine(line)).filter((e) => e);
+
+let scores = completionStrings
+  .map((string) => calculateScore(string))
+  .sort((a, b) => a - b);
+
+console.log(scores);
+console.log(scores[Math.floor(scores.length / 2)]);
